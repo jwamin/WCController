95979fb5dfb20e1de36703767a69d1f8b7c5539f
Joss Manger <jwamin@getready.fios-router.home>
2017-08-19 17:01:32 -0400

    player direction for SK scene with AW

--- WCLive/GameScene.swift
+++ WCLive/GameScene.swift
@@ -15,6 +15,9 @@ class GameScene: SKScene {
     var runInt:Int = 0
     var player:SKShapeNode!
     var isAnimating = false;
+    
+    var distanceOffest:CGFloat = 100;
+    
     override func sceneDidLoad() {
         runInt+=1
         print("hello world",runInt)
@@ -37,19 +40,49 @@ class GameScene: SKScene {
     
     func performAnimation(_ direction:Directions){
         
+        var point:CGPoint = player.position
+        
+        guard let gameScreen = self.view else {
+            fatalError()
+        }
+        
+        switch direction {
+        case .up:
+            point = CGPoint(x: CGFloat(0.0), y: gameScreen.bounds.maxY - distanceOffest)
+        case .down:
+            point = CGPoint(x: CGFloat(0.0), y: -gameScreen.bounds.maxY + distanceOffest)
+        case .left:
+            point = CGPoint(x: -gameScreen.bounds.maxX + distanceOffest, y: CGFloat(0.0))
+        case .right:
+            point = CGPoint(x: gameScreen.bounds.maxX - distanceOffest, y: CGFloat(0.0))
+        case .center:
+            point = CGPoint(x: 0, y: 0)
+        case .downleft:
+            point = CGPoint(x: -gameScreen.bounds.maxX + distanceOffest, y: -gameScreen.bounds.maxY + distanceOffest)
+        case .downright:
+            point = CGPoint(x: gameScreen.bounds.maxX - distanceOffest, y: -gameScreen.bounds.maxY + distanceOffest)
+        case .upright:
+            point = CGPoint(x: gameScreen.bounds.maxX - distanceOffest, y: gameScreen.bounds.maxY - distanceOffest)
+        case .upleft:
+            point = CGPoint(x: -gameScreen.bounds.maxX + distanceOffest, y: gameScreen.bounds.maxY - distanceOffest)
+        }
+        
+        print(point)
         //Switch for direction
         if (!isAnimating){
             isAnimating = true
-            let action = SKAction.move(to: CGPoint(x: -200, y: -200),
+            let action = SKAction.move(to: point,
                                        duration: 1.0)
-            action.timingFunction = {
-                time in
-                return Float(GameScene.timing(t: Double(time), b: 0, c: 1, d: 1.0))
+//            action.timingFunction = {
+//                time in
+//                return Float(GameScene.timing(t: Double(time), b: 0, c: 1, d: 1.0))
+//            }
+            
+            action.timingMode = .easeInEaseOut
+            
+            player.run(action){
+                self.isAnimating = false
             }
-            
-            //action.timingMode = .easeInEaseOut
-            
-            player.run(action)
         }
         
        
@@ -57,28 +90,28 @@ class GameScene: SKScene {
         
     }
     
-    static func timing(t:Double, b:Double, c:Double, d:Double) -> Double{
-        var s:Double=1.70158;
-        var p:Double=0;
-        var a:Double=c;
-        if (t==0){
-            return b;
-        }
-        if ((t/d)==1){
-            return b+c;
-        }
-        if (p == 0){
-            p = d * 0.3;
-        }
-        if (a < abs(c)) {
-            a=c;
-            var s=p/4;
-        } else {
-            var s = p/(2 * .pi) * asin(c/a);
-            return a*pow(2,-10 * t) * sin( (t*d-s)*(2 * .pi)/p ) + c + b;
-        }
-        return 0.0
-    }
+//    static func timing(t:Double, b:Double, c:Double, d:Double) -> Double{
+//        var s:Double=1.70158;
+//        var p:Double=0;
+//        var a:Double=c;
+//        if (t==0){
+//            return b;
+//        }
+//        if ((t/d)==1){
+//            return b+c;
+//        }
+//        if (p == 0){
+//            p = d * 0.3;
+//        }
+//        if (a < abs(c)) {
+//            a=c;
+//            var s=p/4;
+//        } else {
+//            var s = p/(2 * .pi) * asin(c/a);
+//            return a*pow(2,-10 * t) * sin( (t*d-s)*(2 * .pi)/p ) + c + b;
+//        }
+//        return 0.0
+//    }
     
     //    easeOutElastic: function (x, t, b, c, d) {
     //    var s=1.70158;var p=0;var a=c;
